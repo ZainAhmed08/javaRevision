4343ab78a53d8c6a18b04319ba3b71bf2d9bc874
zain ahmed <zainahmed01997@gmail.com>
2023-12-25 17:52:28 +0530

    daily dose , easy java array prob

---
+++ src/com/problems/leetcodeContestProblems/Prob.java
@@ -0,0 +1,9 @@
+package com.problems.leetcodeContestProblems;
+
+public class Prob {
+    public static void main(String[] args) {
+        int i = 10;
+
+        int i1 = i == 10 ? 9 : 10;
+    }
+}
