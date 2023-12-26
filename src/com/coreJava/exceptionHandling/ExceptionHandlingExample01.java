cd4d252af0d2239af7877ad60a422f1a7f95e8f6
zain ahmed <zainahmed01997@gmail.com>
2023-12-26 16:21:04 +0530

    Exception Handling and Multithreading

---
+++ src/com/coreJava/exceptionHandling/ExceptionHandlingExample01.java
@@ -0,0 +1,7 @@
+package com.coreJava.exceptionHandling;
+
+public class ExceptionHandlingExample01 {
+    public static void main(String[] args) {
+        System.out.println("Hello world");
+    }
+}
