93e180767c895e9dfb75f6676516a124bf3572b2
zain ahmed <zainahmed01997@gmail.com>
2024-01-02 16:10:03 +0530

    started with exception handling

--- src/com/coreJava/exceptionHandling/ExceptionHandlingExample01.java
+++ src/com/coreJava/exceptionHandling/ExceptionHandlingExample01.java
@@ -1,7 +1,14 @@
 package com.coreJava.exceptionHandling;
 
 public class ExceptionHandlingExample01 {
+
     public static void main(String[] args) {
-        System.out.println("Hello world");
+
+        try {
+        int num = Integer.parseInt("12tg3");
+        System.out.println("the parsed integer is : " + num);
+        }catch (NumberFormatException nfe){
+            System.out.println("The number String may contain characters");
+        }
     }
 }
---
+++ src/com/veryBasicProblems/Problem1.java
@@ -0,0 +1,12 @@
+package com.veryBasicProblems;
+
+public class Problem1 {
+    public static void main(String[] args) {
+        int arr[] = new int[10];
+
+        for(int i = 0 ; i < arr.length ; i++){
+
+            System.out.println(arr[i]);
+        }
+    }
+}
